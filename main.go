ad41d63d00949bb82984076ea5c4a64bb64ed9c3
Steve Kemp <steve@steve.org.uk>
2019-04-21 21:46:05 +0300

    Added new entry-point

---
+++ main.go
@@ -0,0 +1,30 @@
+// Entry-Point to our code.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+
+	"github.com/google/subcommands"
+)
+
+//
+// Our main-driver.
+//
+// Load the sub-commands we have available, and pass control appropriately.
+//
+func main() {
+
+	subcommands.Register(subcommands.HelpCommand(), "")
+	subcommands.Register(subcommands.FlagsCommand(), "")
+	subcommands.Register(subcommands.CommandsCommand(), "")
+	subcommands.Register(&clientCmd{}, "")
+	subcommands.Register(&serverCmd{}, "")
+
+	flag.Parse()
+	ctx := context.Background()
+	os.Exit(int(subcommands.Execute(ctx)))
+
+}
